faa554ceba6ba6be719fabee6f6a3a5e8c59b649
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-01 06:55:41 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -20,7 +20,9 @@ class Users extends Component {
 
 
         <ul>
-          Users!
+        
+          {users}
+          
           {/* Write code here that displays the usernames of all users in the Redux store */}
           {/* In addition, display the total number of users curently in the store */}
         </ul>
